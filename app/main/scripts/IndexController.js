73ba9ad1cacadf847b4bb9da7d677c5b8d1a601d
Joon Park <JoonPark2017@u.northwestern.edu>
2016-11-19 02:03:58 -0600

    Hook up picture taken by camera to clarifai api.

--- app/main/scripts/IndexController.js
+++ app/main/scripts/IndexController.js
@@ -3,21 +3,26 @@ angular
   .controller('IndexController', function($scope, supersonic) {
     supersonic.ui.tabs.hide();
 
-    $scope.response = '';
-
     var app = new Clarifai.App(
       'SP-cvdgsUVI-mFzs2oAFdWM1GUdkfGk_URKBPGwU',
       'o7s0YcPwIKl6Re-Kn5rYYJptnH4PiFN9oyGg7YC4'
     );
 
-    app.models.predict(Clarifai.GENERAL_MODEL, 'https://samples.clarifai.com/metro-north.jpg').then(
-      function(response) {
-        supersonic.logger.log(response); // Required ???
-        $scope.response = response.data.outputs[0].data.concepts;
-      },
-      function(err) {
-        supersonic.logger.log(err); // Required ???
-        $scope.response = err;
-      }
-    );
+    $scope.response = '';
+
+    supersonic.media.camera.takePicture({
+      destinationType:'dataURL',
+      quality:50
+    }).then(function(result) {
+      app.models.predict(Clarifai.GENERAL_MODEL, result).then(
+        function(response) {
+          supersonic.logger.log(response); // Required ???
+          $scope.response = response.data.outputs[0].data.concepts;
+        },
+        function(err) {
+          supersonic.logger.log(err); // Required ???
+          $scope.response = err;
+        }
+      );
+    });
   });
