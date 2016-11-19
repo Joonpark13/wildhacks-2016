7268087816d9b4885ff06beae351030fbdbac2e0
Joon Park <JoonPark2017@u.northwestern.edu>
2016-11-19 01:26:05 -0600

    Basic Clarify API call.

--- dist/app/main.js
+++ dist/app/main.js
@@ -6,5 +6,23 @@ angular.module('main', [
 angular
   .module('main')
   .controller('IndexController', function($scope, supersonic) {
-    // Controller functionality here
+    supersonic.ui.tabs.hide();
+
+    $scope.response = '';
+
+    var app = new Clarifai.App(
+      'SP-cvdgsUVI-mFzs2oAFdWM1GUdkfGk_URKBPGwU',
+      'o7s0YcPwIKl6Re-Kn5rYYJptnH4PiFN9oyGg7YC4'
+    );
+
+    app.models.predict(Clarifai.GENERAL_MODEL, 'https://samples.clarifai.com/metro-north.jpg').then(
+      function(response) {
+        supersonic.logger.log(response); // Required ???
+        $scope.response = response.data.outputs[0].data.concepts;
+      },
+      function(err) {
+        supersonic.logger.log(err); // Required ???
+        $scope.response = err;
+      }
+    );
   });
